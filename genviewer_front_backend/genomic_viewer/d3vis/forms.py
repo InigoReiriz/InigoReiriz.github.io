ec4ec624c44ec388ceed563957a937461a2ed44e
Inigo Reiriz <inigoreiriz@gmail.com>
2016-11-16 14:51:15 +0100

    updated

--- genviewer_front_backend/genomic_viewer/d3vis/forms.py
+++ genviewer_front_backend/genomic_viewer/d3vis/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from .models import Document
 
 class DocumentForm(forms.ModelForm):
-    class Meta:
-        model = Document
-        fields = ('document', )
+
+    	class Meta:
+        	model = Document
+        	fields = ('document', )
