f8e57125c49bda033532689c59c6732b3a4a4d9d
Inigo Reiriz <inigoreiriz@gmail.com>
2016-11-16 15:18:29 +0100

    updated

--- genviewer_front_backend/genomic_viewer/d3vis/models.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
-# Create your models here. (here we store all the data)
-
-class Document(models.Model):
-    document = models.FileField(upload_to='FilesToProcess/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
